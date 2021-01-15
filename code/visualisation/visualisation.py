6e583aac197f280d03fdd0698003acb6a3a5dcc6
M vd Maas <mylenevdmaas@kpnplanet.nl>
2021-01-15 16:44:01 +0100

    visualize in gif

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -26,12 +26,13 @@ def visualize(sourcefile, outputfile):
     im = plt.imshow(matrix, animated=True)
     ims.append([im])
 
+    # move and add new layout to ims
     for move in moves_set:
         game.move(move)
+        game.board.create_layout()
         matrix = [[ord(letter) for letter in row] for row in game.board.layout]
         im = plt.imshow(matrix, animated=True)
         ims.append([im])
 
-    ani = animation.ArtistAnimation(fig, ims, interval=1, blit=True, repeat_delay=1000)
-
+    ani = animation.ArtistAnimation(fig, ims, interval=10, blit=True, repeat_delay=10000)
     ani.save(f'{outputfile}.gif')
