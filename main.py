b8400f71418e15c13b9f3bf8dd3039840ee59c11
M vd Maas <mylenevdmaas@kpnplanet.nl>
2021-01-21 16:10:10 +0100

    fixed main

--- main.py
+++ main.py
@@ -11,38 +11,38 @@ from code.visualisation.visualisation import visualize
 
 if __name__ == '__main__':
 
-    hybrid2 = Hybrid2(f'data/Rushhour9x9_6.csv', f'output/output_6_85.csv')
-    moves_set = hybrid2.run()
-    print(moves_set)
-    print(len(moves_set))
+    # hybrid2 = Hybrid2(f'data/Rushhour9x9_6.csv', f'output/output_6_85.csv')
+    # moves_set = hybrid2.run()
+    # print(moves_set)
+    # print(len(moves_set))
 
-    # # find algorithm and board input
-    # try:
-    #     algorithm_choice = sys.argv[1]
-    #     board_choice = sys.argv[2]
-    # except: 
-    #     algorithm_choice = "breadthfirst"
-    #     board_choice = '6x6_1'
+    # find algorithm and board input
+    try:
+        algorithm_choice = sys.argv[1]
+        board_choice = sys.argv[2]
+    except: 
+        algorithm_choice = "breadthfirst"
+        board_choice = '6x6_1'
     
-    # # different possible algorithms
-    # algorithms = {
-    # "randomise" : Randomise,
-    # "breadthfirst" : BreadthFirst,
-    # "hybrid": Hybrid
-    # }
+    # different possible algorithms
+    algorithms = {
+    "randomise" : Randomise,
+    "breadthfirst" : BreadthFirst,
+    "hybrid": Hybrid
+    }
     
-    # # run algorithm
-    # moves_set = algorithms[algorithm_choice](f'data/Rushhour{board_choice}.csv').run()
+    # run algorithm
+    moves_set = algorithms[algorithm_choice](f'data/Rushhour{board_choice}.csv').run()
     
-    # # write moves set to outputfile
-    # with open(f'output/output{board_choice}_{algorithm_choice}_{len(moves_set)}.csv', 'w', newline='') as outputfile:
-    #     fieldnames = ['car', 'move']
-    #     writer = csv.writer(outputfile)
-    #     writer.writerow(fieldnames)
-    #     writer.writerows(moves_set)
+    # write moves set to outputfile
+    with open(f'output/output{board_choice}_{algorithm_choice}_{len(moves_set)}.csv', 'w', newline='') as outputfile:
+        fieldnames = ['car', 'move']
+        writer = csv.writer(outputfile)
+        writer.writerow(fieldnames)
+        writer.writerows(moves_set)
     
-    # # # print output
-    # # print(moves_set)
+    # # print output
+    # print(moves_set)
     
-    # # # visualize output
-    # # visualize(f'data/Rushhour{board_choice}.csv', f'output/output{board_choice}_{algorithm_choice}.csv')
+    # # visualize output
+    # visualize(f'data/Rushhour{board_choice}.csv', f'output/output{board_choice}_{algorithm_choice}.csv')
